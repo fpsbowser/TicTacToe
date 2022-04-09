2aaa8c835a1c221dcf19a93ffd0e641a84500f2b
Dakota <prattdakota12@gmail.com>
2022-04-08 20:43:39 -0400

    final commit before improving UI/UX and cleaning up some functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 TicTacToe project utilizing modules and factories.
+Functionally works but it doesn't look too pretty. I may come back to this at a later date and improve UI/UX and also improve
+the functions that check for a winner. I think I should have used loops to loop through the gameboard to check if there was a winner but instead I used a lot of if else statements which isn't the best.
--- script.js
+++ script.js
@@ -1,52 +1,130 @@
 
 const Gameboard = (() => {
-    const gameboard = ['x', 'o', 'x', 'o', '', 'x', '', 'o', ''];
-    console.log(gameboard)
+    const gameboard = ['', '', '', '', '', '', '', '', ''];
+    modal.showModal()
     
     // Get DOM elements
     let gameboardDisplay = document.getElementById('gameboard')
-    console.log(gameboardDisplay)
+    let rowOne = document.getElementsByClassName('rowone')
+    let column = document.getElementsByClassName('column')
+    let rowTwo = document.getElementsByClassName('rowtwo')
+    let columnTwo = document.getElementsByClassName('column-two')
+    let rowThree = document.getElementsByClassName('rowthree')
+    let columnThree = document.getElementsByClassName('column-three')
+    let diagonal = document.getElementsByClassName('diagonal')
+    let secondDiagonal = document.getElementsByClassName('diagonal-two')
+    const resetBtn = document.getElementById('resetButton')
+    const form = document.getElementById('modal-form')
+    let playerOneToMove = true;
 
-    // let section = document.getElementById('section')
-    // console.log(section)
 
     // Bind events
-    gameboardDisplay.addEventListener('click', (e) => {
-        addMarker(e);
+    gameboardDisplay.addEventListener('click', function(e) {
+        const index = [...this.children].filter(el => el.id.indexOf('section') > -1).indexOf(e.target)
+        addMarker(e, index)
     })
-
+    form.addEventListener('submit', function() {
+        playerOne = Player(document.getElementById('playeronename').value, 'X')
+        playerTwo = Player(document.getElementById('playertwoname').value, 'O')
+        form.reset()
+        return playerOne, playerTwo;
+    })
+    resetBtn.addEventListener('click', () => {
+        resetGame()
+    })
+    
     // Player factory
     const Player = (name, selection) => {
         return {name, selection};
 }
-    const dakota = Player('Dakota', 'x');
-    console.log(dakota.name)
-    console.log(dakota.selection)
-
     // Functions
-    function addMarker(event) {
-        console.log(event.target.id)
+    function addMarker(e, sectionToMark) {
+        if (playerOneToMove === true && e.target.innerHTML === "") {
+            gameboard.splice(sectionToMark, 1, playerOne.selection)
+            playerOneToMove = false
+        } else if (playerOneToMove === false && e.target.innerHTML === "") {
+            gameboard.splice(sectionToMark, 1, playerTwo.selection)
+            playerOneToMove = true
+        }
         render()
     }
 
     function render() {
         // Loop through gameboard array and update textcontent with array values
-        console.log('render function called')
+        // console.log(gameboard)
         for (let i = 0; i < gameboard.length; i++) {
-            // console.log(gameboard[i])
             const selection = document.querySelector(`#gameboard :nth-child(${i + 1})`)
-            // console.log(selection)
             selection.innerHTML = gameboard[i]
+        }
+        analyzeGame()
+    }
+
+    function displayWinner(player) {
+        console.log(`Winner: ${player.name}`)
+        alert(`Winner: ${player.name}!`)
+    }
+
+    function tieGame() {
+        alert(`You tied!`)
+        resetGame()
+    }
+
+    function analyzeGame() {
+            gameboard.includes('') ? null : tieGame()
+            checkIfThreeInARow()
+            checkIfThreeInAColumn()
+            checkIfDiagonalWin()
+        }
+
+    function resetGame() {
+        playerOneToMove = true
+        for (let i = 0; i < gameboard.length; i++) {
+            gameboard.splice(i, 1, '')
+        }
+        render()
+    }
+
+    function checkIfThreeInARow() {
+        if (rowOne[0].innerHTML === 'X' && rowOne[1].innerHTML === 'X' && rowOne[2].innerHTML === 'X' ||
+            rowOne[0].innerHTML === 'O' && rowOne[1].innerHTML === 'O' && rowOne[2].innerHTML === 'O') {
+                rowOne[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo);
+                resetGame()
+        } else if (rowTwo[0].innerHTML === 'X' && rowTwo[1].innerHTML === 'X' && rowTwo[2].innerHTML === 'X' ||
+            rowTwo[0].innerHTML === 'O' && rowTwo[1].innerHTML === 'O' && rowTwo[2].innerHTML === 'O') {
+                rowTwo[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
+        } else if (rowThree[0].innerHTML === 'X' && rowThree[1].innerHTML === 'X' && rowThree[2].innerHTML === 'X' ||
+            rowThree[0].innerHTML === 'O' && rowThree[1].innerHTML === 'O' && rowThree[2].innerHTML === 'O') {
+                rowThree[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
         }
     }
 
-    
+    function checkIfThreeInAColumn() {
+        if (column[0].innerHTML === 'X' && column[1].innerHTML === 'X' && column[2].innerHTML === 'X' ||
+            column[0].innerHTML === 'O' && column[1].innerHTML === 'O' && column[2].innerHTML === 'O') {
+                column[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo);
+                resetGame()
+        } else if (columnTwo[0].innerHTML === 'X' && columnTwo[1].innerHTML === 'X' && columnTwo[2].innerHTML === 'X' ||
+            columnTwo[0].innerHTML === 'O' && columnTwo[1].innerHTML === 'O' && columnTwo[2].innerHTML === 'O') {
+                columnTwo[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
+        } else if (columnThree[0].innerHTML === 'X' && columnThree[1].innerHTML === 'X' && columnThree[2].innerHTML === 'X' ||
+            columnThree[0].innerHTML === 'O' && columnThree[1].innerHTML === 'O' && columnThree[2].innerHTML === 'O') {
+                columnThree[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
+        }
+    }
 
+    function checkIfDiagonalWin() {
+        if (diagonal[0].innerHTML === 'X' && diagonal[1].innerHTML === 'X' && diagonal[2].innerHTML === 'X' ||
+            diagonal[0].innerHTML === 'O' && diagonal[1].innerHTML === 'O' && diagonal[2].innerHTML === 'O') {
+                diagonal[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
+        } else if (secondDiagonal[0].innerHTML === 'X' && secondDiagonal[1].innerHTML === 'X' && secondDiagonal[2].innerHTML === 'X' ||
+            secondDiagonal[0].innerHTML === 'O' && secondDiagonal[1].innerHTML === 'O' && secondDiagonal[2].innerHTML === 'O') {
+                secondDiagonal[0].innerHTML === 'X' ? displayWinner(playerOne) : displayWinner(playerTwo)
+                resetGame()
+        }
+    }
 })();
-
-// console.log(Gameboard.gameboard);
-
-// Player factory
-
-
-
